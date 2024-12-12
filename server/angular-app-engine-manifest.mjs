26d4058a3c094586fc45d938a54a193fbdb1768a
girip <giriprasathvm2002@gmail.com>
2024-12-12 16:01:59 +0530

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: 'https://VMGiriprasath.github.io/Portfolio3/index.html',
+  basePath: 'https://VMGiriprasath.github.io/Portfolio3/',
   entryPoints: {
     '': () => import('./main.server.mjs')
   },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,12 +2,12 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'https://VMGiriprasath.github.io/Portfolio3/index.html',
+  baseHref: 'https://VMGiriprasath.github.io/Portfolio3/',
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 7697, hash: '6aac0565565c68871847a5d656f90ea7bd95055a8c2770782d692012889a0a70', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 8024, hash: 'b6b30bfc8cadf1e8b32b605a395750a52998f1d38287e64a10b808500826e87e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 7687, hash: '54654e4f2b6ef8d18631e6ec1f082c6ece3c8942cffcce4bcfa8ef71355a4c19', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 8014, hash: 'e605a3c22611cee251891c5ac7f1782d870744ba3fe5b45a17478c69580f3938', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5XVWDKWT.css': {size: 105, hash: 'RdUNBt6AY6U', text: () => import('./assets-chunks/styles-5XVWDKWT_css.mjs').then(m => m.default)}
   },
 };
